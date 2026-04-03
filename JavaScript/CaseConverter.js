325a3de619622d20289d8dce6b6af95a81b01bf5
himaindla576 <himaindla576@gmail.com>
2026-04-03 16:36:40 +0530

    uppercase

---
+++ JavaScript/CaseConverter.js
@@ -0,0 +1,6 @@
+document.getElementById("btnUpperCase").addEventListener("click",function(){
+    debugger;
+    var name=document.getElementById("txtName").value
+    document.getElementById("pResult").innerHTML=name.toUpperCase();
+
+});
