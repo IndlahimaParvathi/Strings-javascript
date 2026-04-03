07d22325a79cead9133dc9185ed4aaa3211a470a
himaindla576 <himaindla576@gmail.com>
2026-04-03 15:28:24 +0530

    buliding js

---
+++ JavaScript/StringArrays.js
@@ -0,0 +1,5 @@
+document.getElementById("btnPrintFirst").addEventListener("click",function(){
+    debugger;
+    var name= getElementById("txtName").value
+    document.getElementById("pResult").innerHTML=name[0];
+});
