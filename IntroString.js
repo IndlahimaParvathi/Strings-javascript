5068b9ccffe0657ec368d74516dfc9b4ec3fa618
himaindla576 <himaindla576@gmail.com>
2026-04-03 11:38:53 +0530

    buliding js

--- IntroString.js
+++ IntroString.js
@@ -1,3 +1,11 @@
 document.getElementById("btnSubmit").addEventListener("click", function(){
-    debu
-})
+    debugger;
+   var firstName= document.getElementById("txtFirstName").value
+   var secondName= document.getElementById("txtSecondName").value
+   var email= document.getElementById("txtEmail").value
+
+   alert(firstName);
+   alert(secondName);
+    alert(email);
+   
+});
