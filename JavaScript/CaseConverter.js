9ba122a8f1fcb9e1cbe9154dfadeb14c8a6a2681
himaindla576 <himaindla576@gmail.com>
2026-04-03 17:07:46 +0530

    adding start and end to js

--- JavaScript/CaseConverter.js
+++ JavaScript/CaseConverter.js
@@ -20,3 +20,18 @@ document.getElementById("btnTrim").addEventListener("click",function(){
     document.getElementById("pResult").innerHTML=name.trim();
 
 });
+
+document.getElementById("btnTrimStart").addEventListener("click",function(){
+    debugger;
+    var name=document.getElementById("txtName").value
+    document.getElementById("pResult").innerHTML=name.trimStart();
+
+});
+
+
+document.getElementById("btnTrimEnd").addEventListener("click",function(){
+    debugger;
+    var name=document.getElementById("txtName").value
+    document.getElementById("pResult").innerHTML=name.trimEnd();
+
+});
