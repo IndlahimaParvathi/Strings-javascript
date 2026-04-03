364b55a4eefa1b774bc1ec12ab71c4445530c16c
himaindla576 <himaindla576@gmail.com>
2026-04-03 16:39:26 +0530

    lowercase

--- JavaScript/CaseConverter.js
+++ JavaScript/CaseConverter.js
@@ -3,4 +3,13 @@ document.getElementById("btnUpperCase").addEventListener("click",function(){
     var name=document.getElementById("txtName").value
     document.getElementById("pResult").innerHTML=name.toUpperCase();
 
+
+
+});
+
+    document.getElementById("btnLowerCase").addEventListener("click",function(){
+    debugger;
+    var name=document.getElementById("txtName").value
+    document.getElementById("pResult").innerHTML=name.toLowerCase();
+
 });
