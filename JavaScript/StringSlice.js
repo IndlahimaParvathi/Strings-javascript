5851473f0814658eb67228909d6ffffb880d8d3a
himaindla576 <himaindla576@gmail.com>
2026-04-04 13:21:08 +0530

    buliding html

--- JavaScript/StringSlice.js
+++ JavaScript/StringSlice.js
@@ -1,5 +1,7 @@
-  document.getElementById("btnCompare").addEventListener("click",function(){
+  document.getElementById("btnSlice").addEventListener("click",function(){
     debugger;
     var name=document.getElementById("txtName").value
-    document.getElementById("pResult").innerHTML=name.slice(2,6);
+    var startnumber=document.getElementById("txtStartNumber").value
+      var Endnumber=document.getElementById("txtEndNumber").value
+    document.getElementById("pResult").innerHTML=name.slice(startnumber,Endnumber);
   });
