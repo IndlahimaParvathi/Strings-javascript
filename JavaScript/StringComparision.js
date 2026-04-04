34aa43e328ee382018570a7fb325cac2b1541347
himaindla576 <himaindla576@gmail.com>
2026-04-04 11:37:31 +0530

    buliding js

---
+++ JavaScript/StringComparision.js
@@ -0,0 +1,13 @@
+document.getElementById("btnCompare").addEventListener("click",function(){
+debugger;
+var name1=document.getElementById("txtName1").value
+var name2=document.getElementById("txtName2").value
+if(name1.toUpperCase().trim()==name2.toUpperCase().trim()){
+    document.getElementById("pResult").innerHTML= "both are same."
+}
+else{
+   document.getElementById("pResult").innerHTML= "both are different."
+ 
+}
+
+});
