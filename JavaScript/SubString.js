381bc5a8855ab5a7077359df4b78ece859441993
himaindla576 <himaindla576@gmail.com>
2026-04-11 22:27:24 +0530

    buliding js

--- JavaScript/SubString.js
+++ JavaScript/SubString.js
@@ -1,7 +1,7 @@
-  document.getElementById("btnSlice").addEventListener("click",function(){
+  document.getElementById("btnSubString").addEventListener("click",function(){
     debugger;
     var name=document.getElementById("txtName").value
     var startnumber=document.getElementById("txtStartNumber").value
       var Endnumber=document.getElementById("txtEndNumber").value
-    document.getElementById("pResult").innerHTML=name.subString(startnumber,Endnumber);
+    document.getElementById("pResult").innerHTML=name.substring(startnumber,Endnumber);
   });
