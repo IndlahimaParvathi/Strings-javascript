d1e01ca71f0925f9d617cc7df34a8ef4c9235c4e
himaindla576 <himaindla576@gmail.com>
2026-04-12 16:21:47 +0530

    buliding  js

--- JavaScript/EmailVerification.js
+++ JavaScript/EmailVerification.js
@@ -2,12 +2,20 @@ document.getElementById("btnCheck").addEventListener("click", function () {
     debugger;
     var email = document.getElementById("txtEmail").value
     if (email.includes("@") && email.includes('.')) {
-        document.getElementById("pResult").innerHTML = "Email is valid"
+      
+        if (email.indexOf("@") < (email.indexOf('.'))) {
+
+
+            document.getElementById("pResult").innerHTML = "Email is valid"
+        }
+        else {
+            document.getElementById("pResult").innerHTML = "In valid Email"
+        }
+
     }
     else {
         document.getElementById("pResult").innerHTML = "In valid Email"
     }
-
-
-
 });
+
+
