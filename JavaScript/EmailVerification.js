a902acb06d13e6c38fc480f0db05186ac52bdc8f
himaindla576 <himaindla576@gmail.com>
2026-04-12 16:05:56 +0530

    bug the mistakes

--- JavaScript/EmailVerification.js
+++ JavaScript/EmailVerification.js
@@ -2,10 +2,10 @@ document.getElementById("btnCheck").addEventListener("click", function () {
     debugger;
     var email = document.getElementById("txtEmail").value
     if (email.includes("@") && email.includes('.')) {
-        document.getElementById("").innerHTML = "Email is valid"
+        document.getElementById("pResult").innerHTML = "Email is valid"
     }
     else {
-        document.getElementById("").innerHTML = "In valid Email"
+        document.getElementById("pResult").innerHTML = "In valid Email"
     }
 
 
