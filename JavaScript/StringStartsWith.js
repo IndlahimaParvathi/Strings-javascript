bbeea0cfc74d6f6c1bd2264f69428f5def2c72d9
himaindla576 <himaindla576@gmail.com>
2026-04-12 14:26:32 +0530

    bug the mistakes

--- JavaScript/StringStartsWith.js
+++ JavaScript/StringStartsWith.js
@@ -2,5 +2,5 @@ document.getElementById("btnFind").addEventListener("click",function(){
     debugger;
   var stringName=document.getElementById("txtStringName").value
     var search=document.getElementById("txtSearch").value
-document.getElementById("pResult").innerHTML=stringName.startWith(search)
+document.getElementById("pResult").innerHTML=stringName.startsWith(search)
      });
