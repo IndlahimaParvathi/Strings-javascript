04666e3d2907f1fc5cea04867dc5aa597b4f2a25
himaindla576 <himaindla576@gmail.com>
2026-04-12 13:13:29 +0530

    buliding js

---
+++ JavaScript/StringindexOff.js
@@ -0,0 +1,6 @@
+ document.getElementById("btnFind").addEventListener("click",function(){
+    debugger;
+  var stringName=document.getElementById("txtStringName").value
+    var Search=document.getElementById("txtSearch").value
+    
+     });
