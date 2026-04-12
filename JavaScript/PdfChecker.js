bbcc5012f9c0ac1cf4d4080a54d93a5bf8ddfbc8
himaindla576 <himaindla576@gmail.com>
2026-04-12 16:49:27 +0530

    buliding js

--- JavaScript/PdfChecker.js
+++ JavaScript/PdfChecker.js
@@ -1,5 +1,11 @@
 document.getElementById("btnFind").addEventListener("click",function(){
     debugger;
-  var stringName=document.getElementById("txtStringName").value
-    
-     });
+  var fileName=document.getElementById("txtFileName").value
+  if(fileName.endsWith('.doc')){
+    document.getElementById('pResult').innerHTML= 'It is PDF'
+  }
+  else{
+    document.getElementById('pResult').innerHTML= 'It not is PDF'
+  }
+
+  });
