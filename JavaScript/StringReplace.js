51624df799cfc6b4de22cc5d356b176423b9a2c5
himaindla576 <himaindla576@gmail.com>
2026-04-14 11:07:46 +0530

    bug the mistakes

--- JavaScript/StringReplace.js
+++ JavaScript/StringReplace.js
@@ -1,6 +1,9 @@
 document.getElementById("btnSubmit").addEventListener("click",function(){
     debugger;
     var inputTest=document.getElementById("txtInputTest").value;
-     var replaceTest=Number(document.getElementById("txtReplaceValue").value);
-     document.getElementById("pResult").innerHTML=(inputTest +"\n").replace(replaceTest);
+     var replaceWord=document.getElementById("txtReplaceWord").value;
+     document.getElementById("pResult").innerHTML=inputTest.replaceAll("hima",replaceWord);
 });
+
+
+
