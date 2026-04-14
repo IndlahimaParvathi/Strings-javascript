a20b01240579ac4f7dbfa81e8366196695e8bddc
himaindla576 <himaindla576@gmail.com>
2026-04-14 12:23:11 +0530

    bug the mistrakes

--- JavaScript/SecuredWebsiteChecker.js
+++ JavaScript/SecuredWebsiteChecker.js
@@ -1,11 +1,10 @@
-document.getElementById("btnFind").addEventListener("click",function(){
+document.getElementById("btnCheck").addEventListener("click", function () {
     debugger;
-  var fileName=document.getElementById("txtFileName").value
-  if(fileName.endsWith('http')){
-    document.getElementById('pResult').innerHTML= 'It is Secured Website Checker'
-  }
-  else{
-    document.getElementById('pResult').innerHTML= 'It not is Secured Website Checker'
-  }
-
-  });
+    var websiteName = document.getElementById("txtWebsiteName").value
+    if (websiteName.startsWith('http')) {
+        document.getElementById('pResult').innerHTML = 'It is Secured Website'
+    }
+    else {
+        document.getElementById('pResult').innerHTML = 'It not is Secured Website'
+    }
+});
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Data Type String
+Strings
+
