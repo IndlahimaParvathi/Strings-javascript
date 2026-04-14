e4af6c64141d0290f5103ae6aba13692aed14cc4
himaindla576 <himaindla576@gmail.com>
2026-04-14 11:47:11 +0530

    buliding js

--- JavaScript/SecuredWebsiteChecker.js
+++ JavaScript/SecuredWebsiteChecker.js
@@ -1,11 +1,11 @@
 document.getElementById("btnFind").addEventListener("click",function(){
     debugger;
   var fileName=document.getElementById("txtFileName").value
-  if(fileName.endsWith('.pdf')){
-    document.getElementById('pResult').innerHTML= 'It is PDF'
+  if(fileName.endsWith('http')){
+    document.getElementById('pResult').innerHTML= 'It is Secured Website Checker'
   }
   else{
-    document.getElementById('pResult').innerHTML= 'It not is PDF'
+    document.getElementById('pResult').innerHTML= 'It not is Secured Website Checker'
   }
 
   });
