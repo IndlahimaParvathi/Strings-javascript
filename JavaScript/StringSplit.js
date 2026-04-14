c62e546c4e7f6f8ef0cfb53bbae03c4ed514b54b
himaindla576 <himaindla576@gmail.com>
2026-04-14 11:41:00 +0530

    bulinking js

---
+++ JavaScript/StringSplit.js
@@ -0,0 +1,9 @@
+document.getElementById("btnSubmit").addEventListener("click",function(){
+    debugger;
+    var input=document.getElementById("txtInput").value;
+     
+     document.getElementById("pResult").innerHTML=input.split(".");
+});
+
+
+
