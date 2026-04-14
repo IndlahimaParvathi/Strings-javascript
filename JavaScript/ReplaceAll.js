7d157bb9ad61090db21538bdc442581d7d264d9c
himaindla576 <himaindla576@gmail.com>
2026-04-14 11:33:10 +0530

    debug the mistakes

--- JavaScript/ReplaceAll.js
+++ JavaScript/ReplaceAll.js
@@ -2,8 +2,8 @@ document.getElementById("btnSubmit").addEventListener("click",function(){
     debugger;
     var sentances=document.getElementById("txtSentences").value;
      var Word=document.getElementById("txtWord").value;
-     var replaceWord=document.getElementById("txtReplaceWord");
-     document.getElementById("pResult").innerHTML=inputTest.replaceAll(Word,replaceWord);
+     var replaceAll=document.getElementById("txtReplaceAll").value;
+     document.getElementById("pResult").innerHTML=sentances.replaceAll(Word,replaceAll);
 });
 
 
