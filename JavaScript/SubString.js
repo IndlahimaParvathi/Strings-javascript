2a633008d224be73915f517b719646390ebb6caf
himaindla576 <himaindla576@gmail.com>
2026-05-11 23:44:41 +0530

    removing debuger

--- JavaScript/SubString.js
+++ JavaScript/SubString.js
@@ -1,5 +1,5 @@
   document.getElementById("btnSubString").addEventListener("click",function(){
-    debugger;
+    
     var name=document.getElementById("txtName").value
     var startnumber=document.getElementById("txtStartNumber").value
       var Endnumber=document.getElementById("txtEndNumber").value
