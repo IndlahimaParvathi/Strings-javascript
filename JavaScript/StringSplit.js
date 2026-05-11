a855d8c0bb57589d4b1507e49b48fde17b452c83
himaindla576 <himaindla576@gmail.com>
2026-05-11 23:43:38 +0530

    removing

--- JavaScript/StringSplit.js
+++ JavaScript/StringSplit.js
@@ -1,5 +1,5 @@
 document.getElementById("btnSubmit").addEventListener("click",function(){
-    debugger;
+
     var input=document.getElementById("txtInput").value;
      
      document.getElementById("pResult").innerHTML=input.split(".");
